4554cea0a0a5392ef06619a2072b4fa187b74c8d
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-05-09 18:08:54 -0300

    adicionando urls e views

--- Insta/app/views.py
+++ Insta/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from app.models import usuario, post
 
-# Create your views here.
+def index(request, arroba):
+    try: 
+        arroba = usuario.objects.get(pk=arroba)
+        publi = post.objects.filter(arroba=arroba)
+    except arroba.DoesNotExist:
+        raise Http404('Arroba não encontrado')
+    return render(request, 'app/index.html', {'post' : publi, 'arroba': arroba})
