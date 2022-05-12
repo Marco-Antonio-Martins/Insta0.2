e7a532f46071234f3d95d67d775469fb3895584d
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-05-12 16:46:25 -0300

    Alterações básicas

--- Insta/app/views.py
+++ Insta/app/views.py
@@ -5,7 +5,10 @@ from app.models import usuario, post
 def index(request, arroba):
     try: 
         arroba = usuario.objects.get(pk=arroba)
-        publi = post.objects.filter(arroba=arroba)
     except arroba.DoesNotExist:
-        raise Http404('Arroba não encontrado')
+        raise Http404('Arroba não encontrado') #n funfa
+    try:
+        publi = post.objects.filter(arroba=arroba)
+    except publi.DoesNotExist:
+        raise Http404('Publicação não encontrada')#n funfa
     return render(request, 'app/index.html', {'publi' : publi, 'arroba': arroba})
